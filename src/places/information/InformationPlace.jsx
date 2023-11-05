c8bbc539a68f36659197249d2355af703e47a7be
electrum <kachalina.kate@mail.ru>
2023-11-05 20:09:34 +0300

    05-11-2023:20:09

--- src/places/information/InformationPlace.jsx
+++ src/places/information/InformationPlace.jsx
@@ -1,10 +1,19 @@
 import {Text, View, Image} from 'react-native';
-
+import headers from '../../data/headers';
+import Layout from '../../components/layout/Layout';
+import { COLORS } from '../../styles/variables';
 import styles from './InformationPlace.style';
 
 const InformationPlace = ({navigation}) => {
+  const headerData = headers.find(
+    headerItem => headerItem.classHeader === 'information',
+  );
 
-    return <Text>InformationPlace</Text>
-}
+  return (
+    <Layout headerData={headerData} navigation={navigation} background={COLORS.accentBg}>
+      <View></View>
+    </Layout>
+  );
+};
 
 export default InformationPlace;
--- src/places/sport/SportPlace.jsx
+++ src/places/sport/SportPlace.jsx
@@ -1,10 +1,22 @@
 import {Text, View, Image} from 'react-native';
-
+import headers from '../../data/headers';
+import Layout from '../../components/layout/Layout';
+import {COLORS} from '../../styles/variables';
 import styles from './SportPlace.style';
 
-const SportPlace = () => {
+const SportPlace = ({navigation}) => {
+  const headerData = headers.find(
+    headerItem => headerItem.classHeader === 'sport',
+  );
 
-    return <Text>SportPlace</Text>
-}
+  return (
+    <Layout
+      headerData={headerData}
+      navigation={navigation}
+      background={COLORS.accentBg}>
+      <View></View>
+    </Layout>
+  );
+};
 
 export default SportPlace;
