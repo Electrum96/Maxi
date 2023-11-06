eab8b27a7501baaeae59ba8da2ec746a524341f4
electrum <kachalina.kate@mail.ru>
2023-11-06 13:06:30 +0300

    06-11-2023:13:06

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 import {Text, View, Image, TouchableOpacity} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-
+import {useNavigationState} from '@react-navigation/native';
 import styles from './Header.style';
 
-const Header = ({icon, color, title, iconTwo, route,  navigation}) => {
-//  const {} = useLocattion
-  const goBag = () => navigation.push('Bag')
-  const goRoute = () => navigation.push(route)
+const Header = ({icon, color, title, iconTwo, route, navigation}) => {
+  const routes = useNavigationState(state => state.routes);
+  const currentRoute = routes[routes.length - 1].name;
+  const goBag = () => navigation.push('Bag');
+  const goRoute = () => {
+    if (currentRoute === 'Book') {
+      navigation.push('Main');
+    } else {
+      navigation.goBack();
+    }
+  };
 
   return (
     <View style={styles.headerWrp}>
@@ -14,7 +21,7 @@ const Header = ({icon, color, title, iconTwo, route,  navigation}) => {
         <FontAwesomeIcon icon={icon} color={color} size={25} />
       </TouchableOpacity>
 
-      {title && <Text  style={{...styles.title, color: color}}>{title}</Text>}
+      {title && <Text style={{...styles.title, color: color}}>{title}</Text>}
 
       <TouchableOpacity onPress={goBag}>
         {iconTwo && <FontAwesomeIcon icon={iconTwo} color={color} size={25} />}
